2c561731278d5bc79e28cdae5b5c658cf7919508
Jerry Vinokurov <jerry@adaptivemgmt.com>
2018-06-18 12:36:06 -0400

    rename readme

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version='0.1.0',
     packages=['crest'],
     license='LICENSE.txt',
-    long_description=open('README.md').read(),
+    long_description=open('README').read(),
     install_requires=[
         'requests==2.18.4',
         'jsonschema==2.6.0'
