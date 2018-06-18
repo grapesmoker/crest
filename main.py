77ec5eee03a62834eda87ae19064d953af973a45
Jerry Vinokurov <jerry@adaptivemgmt.com>
2018-06-18 11:53:20 -0400

    main

--- main.py
+++ main.py
@@ -2,13 +2,40 @@ from pprint import pprint
 
 from crest.builder import Get, RESTInterface
 from crest.client import Client
+from crest.schema import JSONSchema
+
+
+result_schema = {
+    'title': 'reqres_test_schema',
+    'type': 'object',
+    'properties': {
+        'data': {
+            'type': 'object',
+            'properties': {
+                'avatar': {
+                    'type': 'string'
+                },
+                'first_name': {
+                    'type': 'string'
+                },
+                'last_name': {
+                    'type': 'string'
+                },
+                'id': {
+                    'type': 'integer'
+                }
+            }
+        }
+    }
+}
 
 if __name__ == '__main__':
 
     class TestREST(RESTInterface):
 
-        user = Get('users/{id}', None, 'api')
-        users = Get('users', None, 'api')
+        api_base = 'api'
+        user = Get('users/{id}', JSONSchema(result_schema))
+        users = Get('users', None)
 
     client = Client('http://reqres.in')
 
