0894cb79de8694949482072fe570ccd4406fa3f9
Jerry Vinokurov <jerry@adaptivemgmt.com>
2018-06-18 12:29:30 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+
+setup(
+    name='cREST',
+    version='0.1.0',
+    packages=['crest'],
+    license='LICENSE.txt',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'requests==2.18.4',
+        'jsonschema==2.6.0'
+    ],
+)
