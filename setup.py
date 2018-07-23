d8c916c2d49594379d595e731ccb974fe2091b91
Jerry Vinokurov <jerry@adaptivemgmt.com>
2018-07-23 16:12:04 -0400

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='cREST',
-    version='0.1.3',
+    version='0.1.4',
     packages=['crest', 'crest.tests'],
     license='LICENSE.txt',
     long_description=open('README.txt').read(),
